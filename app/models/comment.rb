7771ffe82c8bf0ff5d955685d074bb5e954b294a
Agustin Soria <soriaagustin8@gmail.com>
2023-11-01 19:38:29 -0300

    Adding comment model

---
+++ app/models/comment.rb
@@ -0,0 +1,13 @@
+class Comment < ApplicationRecord
+    # Associations
+    belongs_to :user
+    belongs_to :post
+  
+    # Callbacks
+    after_save :update_post_comments_counter
+  
+    # Methods
+    def update_post_comments_counter
+      post.increment!(:comments_counter)
+    end
+  end
