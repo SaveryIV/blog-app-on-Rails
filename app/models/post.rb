563b569b0e8f08766da9ed46e9a3d7771811f824
Agustin Soria <soriaagustin8@gmail.com>
2023-11-01 20:05:13 -0300

    fixing linters

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,23 +1,23 @@
 class Post < ApplicationRecord
-    # Associations
-    has_many :comment
-    has_many :like
-    belongs_to :author, class_name: 'User'
-  
-    # Validations
-    validates :title, presence: true, length: { maximum: 250 }
-    validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-    validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-  
-    # Callbacks
-    after_save :update_user_posts_counter
-  
-    # Methods
-    def update_user_posts_counter
-      author.increment!(:posts_counter)
-    end
-  
-    def five_most_recent_comments
-      Comment.where(post_id: id).order(created_at: :desc).limit(5)
-    end
+  # Associations
+  has_many :comment
+  has_many :like
+  belongs_to :author, class_name: 'User'
+
+  # Validations
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  # Callbacks
+  after_save :update_user_posts_counter
+
+  # Methods
+  def update_user_posts_counter
+    author.increment!(:posts_counter)
   end
+
+  def five_most_recent_comments
+    Comment.where(post_id: id).order(created_at: :desc).limit(5)
+  end
+end
