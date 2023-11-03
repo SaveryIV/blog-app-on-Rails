e01bb6305dce6ff6d7b33a2cdf5c1b2d536dfb8f
Agustin Soria <soriaagustin8@gmail.com>
2023-11-03 17:41:01 -0300

    Update migrations

--- db/migrate/20231101182340_create_post.rb
+++ db/migrate/20231101182340_create_post.rb
@@ -1,12 +1,11 @@
 class CreatePost < ActiveRecord::Migration[7.1]
   def change
     create_table :posts do |t|
+      t.references :author, foreign_key: { to_table: :users}
       t.string :title
       t.text :text
-      t.integer :comments_counter
       t.integer :likes_counter
-      t.references :author, null: false, foreign_key: { to_table: :users}
-      
+      t.integer :comments_counter
       t.timestamps
     end
   end
