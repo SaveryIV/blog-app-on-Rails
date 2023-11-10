0b15ff470bcbcef495513bdbcf37f4c792114dd4
Agustin Soria <soriaagustin8@gmail.com>
2023-11-10 03:44:48 -0300

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root "user#index"
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show]
+    resources :posts, only: [:index, :new, :create, :show]
+    resources :comments, only: [:new, :create]
+    resources :likes, only: [:create]
   end
 end
