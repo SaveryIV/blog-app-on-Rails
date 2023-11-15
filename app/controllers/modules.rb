47a2fa2c8ecba7532ad9b87eaa0849c9e9400d46
Agustin Soria <soriaagustin8@gmail.com>
2023-11-15 03:22:34 -0300

    fixing linters

--- app/controllers/modules.rb
+++ app/controllers/modules.rb
@@ -1,9 +1,9 @@
 module Counter
-    def comments_and_likes_counter(counter)
-      if counter.nil?
-        0
-      else
-        counter
-      end
+  def comments_and_likes_counter(counter)
+    if counter.nil?
+      0
+    else
+      counter
     end
+  end
 end
--- spec/features/placeholder.rb
+++ spec/features/placeholder.rb
@@ -1,6 +1,6 @@
 module TestPlaceholders
-    def placeholder
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt \
+  def placeholder
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt \
         labore et dolore magna aliqua. Ut porttitor leo a diam sollicitudin tempor id eu nisl. \
         Facilisis mauris sit amet massa vitae. Nulla facilisi etiam dignissim diam quis enim. \
         Pulvinar etiam non quam lacus suspendisse faucibus. Aliquet nec ullamcorper sit amet risus \
@@ -9,7 +9,7 @@ module TestPlaceholders
         leo. Tempor orci dapibus ultrices in iaculis nunc sed augue lacus. Quis vel eros donec ac \
         odio tempor orci. Dignissim sodales ut eu sem integer. Maecenas pharetra convallis posuere \
         morbi leo urna molestie at.\
-  
+
         Sociis natoque penatibus et magnis dis parturient montes nascetur ridiculus. Blandit aliquam \
         etiam erat velit scelerisque in dictum non consectetur. Semper auctor neque vitae tempus quam \
         pellentesque nec. Aliquam malesuada bibendum arcu vitae. Sit amet consectetur adipiscing elit \
@@ -17,7 +17,7 @@ module TestPlaceholders
         faucibus scelerisque eleifend donec pretium vulputate. Sollicitudin aliquam ultrices sagittis \
         orci a scelerisque. Magna etiam tempor orci eu lobortis elementum nibh tellus molestie. Dignissim \
         enim sit amet venenatis urna cursus eget.
-  
+
         Sollicitudin aliquam ultrices sagittis orci a scelerisque purus semper. Ipsum nunc aliquet \
         bibendum enim. Ornare lectus sit amet est placerat in egestas erat imperdiet. Suspendisse \
         faucibus interdum posuere lorem ipsum dolor sit amet. Mauris a diam maecenas sed enim ut sem. \
@@ -26,5 +26,5 @@ module TestPlaceholders
         pellentesque massa placerat duis ultricies lacus sed. Tortor condimentum lacinia quis vel eros \
         donec ac odio. Ac turpis egestas integer eget aliquet nibh. Risus nullam eget felis eget nunc \
         lobortis mattis aliquam. Facilisis gravida neque convallis a cras semper auctor neque."
-    end
   end
+end
